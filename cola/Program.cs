6e5cc1357c41637baab086c2004357928c2307eb
jonatan.maxi.ponce@gmail.com <jonatan.maxi.ponce@gmail.com>
2020-04-21 03:40:14 -0300

    metodo enqueue metodo dequeue recorrido de la cola

---
+++ cola/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaramos una cola
+            Queue<int> numeros = new Queue<int>();
+
+            //Agregamos elementos a la cola
+            numeros.Enqueue(1);
+            numeros.Enqueue(2);
+            numeros.Enqueue(3);
+            numeros.Enqueue(4);
+            numeros.Enqueue(5);
+
+            //Recorriendo la cola
+            Console.WriteLine("Recorremos la cola por 1ra vez");
+
+            foreach (int elemento in numeros)
+            {
+                Console.WriteLine(elemento);
+            }
+
+            //Eliminando elemento de la cola
+            numeros.Dequeue();
+
+            //Recorriendo la cola
+            Console.WriteLine("Recorremos la cola con el elemento eliminado");
+            foreach (int elemento in numeros)
+            {
+                Console.WriteLine(elemento);
+            }
+
+        }
+    }
+}
